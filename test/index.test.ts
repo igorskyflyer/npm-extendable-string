b0b894383210e606689c3c1d9f2cfc1354cbe366
igorskyflyer <igor.dvlpr@gmail.com>
2023-05-30 00:16:46 +0200

    Update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,15 @@
 import { assert as chai } from 'chai'
+import { UpperCaseString } from './UpperCaseString'
 
 describe('🧪 extendable-string tests 🧪', () => {
-  it('#1 should return ', () => {
-    //
-  })
+  it('#1 should return "AAAA"', () => {
+    const test = new UpperCaseString('aaaa')
+    chai.equal(test.value, 'AAAA')
+  }) // #1
+
+  it('#2 should return "HELLO WORLD"', () => {
+    const test = new UpperCaseString('aaaa')
+    test.value = 'hello world'
+    chai.equal(test.value, 'HELLO WORLD')
+  }) // #2
 })
