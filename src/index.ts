b25573759c2a1f1405adf62f9eee59f4f6bf51f2
igorskyflyer <igor.dvlpr@gmail.com>
2023-05-29 17:30:18 +0200

    Add API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 
+class ExtendableString extends String {
+  private transformFn: (str: string) => string;
+
+  constructor(value: string, transformFn: (str: string) => string) {
+    super(value);
+    this.transformFn = transformFn;
+  }
+
+  toString(): string {
+    return this.transformFn(this.valueOf());
+  }
+}
