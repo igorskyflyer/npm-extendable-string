ad7b17617dd53af40372fa07e1440d498b1288d2
igorskyflyer <igor.dvlpr@gmail.com>
2023-05-29 17:43:46 +0200

    Export class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-
-class ExtendableString extends String {
-  private transformFn: (str: string) => string;
+export class ExtendableString extends String {
+  private transformFn: (str: string) => string
 
   constructor(value: string, transformFn: (str: string) => string) {
-    super(value);
-    this.transformFn = transformFn;
+    super(value)
+    this.transformFn = transformFn
   }
 
   toString(): string {
-    return this.transformFn(this.valueOf());
+    return this.transformFn(this.valueOf())
   }
 }
